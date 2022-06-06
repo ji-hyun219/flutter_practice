c51d331bfe1adb3200898f23c215fa31c0ebd424
ji-hyun219 <ix219@hanmail.net>
2022-06-06 17:47:24 +0900

    feat(StatelessWidget): Scaffold 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     // runApp 다음에는 항상 MaterialApp 이 들어가야 한다.
-    const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            'Hello World', 
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20.0,
-          ),),
-        ),
-      ),
+    MaterialApp(
+      home: HomeScreen(),
     ),
   );
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello World2',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+    );
+  }
 }
