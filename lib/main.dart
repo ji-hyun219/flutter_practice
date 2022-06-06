dab2ab1679fe896f4789274f3c9ce0e84ea6cf10
ji-hyun219 <ix219@hanmail.net>
2022-06-06 22:34:31 +0900

    fix(Column): 파일 분리 & 칼럼

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,13 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:my_app/screen/home_screen.dart';
+// project_Name/lib 경로를 기준으로 설정한다.
 
 void main() {
   runApp(
     // runApp 다음에는 항상 MaterialApp 이 들어가야 한다.
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     ),
   );
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.orange,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/img/logo.png',
-          ),
-          CircularProgressIndicator(
-            color: Colors.white,
-          ),
-        ],
-      ),
-    );
-  }
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// stelss 로 위젯을 자동 생성할 수 있다.
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  // HomeScreen 에 Key 를 넣어줘도 되고 안 넣어줘도 됨
+  // 넣어주면 부모 Constrctor 에 키 삽입.
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: (Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+              Container(
+                color: Colors.red,
+                width: 50.0,
+                height: 50.0,
+              ),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
