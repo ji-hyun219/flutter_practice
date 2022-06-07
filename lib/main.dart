7348cfefb7e5ca62297c75cdb70b8ba214de59c0
ji-hyun219 <ix219@hanmail.net>
2022-06-07 12:08:08 +0900

    feat(BusinessCard): part1

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/screen/home_screen.dart';
-// project_Name/lib 경로를 기준으로 설정한다.
+import 'package:my_app/screen/business_card.dart';
+// project_name/lib 경로를 기준으로 설정한다.
 
 void main() {
   runApp(
     // runApp 다음에는 항상 MaterialApp 이 들어가야 한다.
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: BusinessCard(),
     ),
   );
 }
