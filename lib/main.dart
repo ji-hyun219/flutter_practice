ea97c24e3be0be8479523595cca0fd3638d957ee
ji-hyun219 <ix219@hanmail.net>
2022-06-07 18:14:57 +0900

    feat(BuildingLayouts): my work

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/screen/home_screen.dart';
 import 'package:my_app/screen/business_card.dart';
+import 'package:my_app/screen/building_layouts.dart';
 // project_name/lib 경로를 기준으로 설정한다.
 
 void main() {
@@ -8,7 +9,7 @@ void main() {
     // runApp 다음에는 항상 MaterialApp 이 들어가야 한다.
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BusinessCard(),
+      home: BuildingLayouts(),
     ),
   );
 }
