bd39c26bef39b379894371dae35c8990e77b101c
ji-hyun219 <ix219@hanmail.net>
2022-06-07 15:39:12 +0900

    feat(BusinessCard): part2

--- lib/screen/business_card.dart
+++ lib/screen/business_card.dart
@@ -9,30 +9,60 @@ class BusinessCard extends StatelessWidget {
       body: SafeArea(
         bottom: false,
         child: Container(
-            margin: const EdgeInsets.all(15.0),
-            padding: const EdgeInsets.only(right: 10),
-            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center, // center == 디폴트 값
-              // a Row's cross axis is vertical
-              // a Column's cross axis is horizontal.
-              children: [
-                Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Icon(Icons.account_circle, size: 50)),
-                Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Flutter Jihyun',
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      const Text('Experienced App Developer'),
-                    ]),
-              ],
-            )),
+          width: 300,
+          margin: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.only(right: 10),
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment:
+                    CrossAxisAlignment.center, // center == 디폴트 값
+                // a Row's cross axis is vertical
+                // a Column's cross axis is horizontal.
+                children: [
+                  const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(Icons.account_circle, size: 50)),
+                  Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Flutter Jihyun',
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                        const Text('Experienced App Developer'),
+                      ]),
+                ],
+              ),
+              Column(children: [
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text('성동구 상원길 80'),
+                    Text('010-0000-0000'),
+                  ],
+                ),
+                const SizedBox(height: 16),
+              ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  Icon(
+                    Icons.accessibility,
+                  ),
+                  Icon(Icons.timer),
+                  Icon(Icons.phone_android),
+                  Icon(Icons.phone_iphone),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
