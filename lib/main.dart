b27759d8ae84f094b00e8acf349f6f8128677e05
ji-hyun219 <ix219@hanmail.net>
2022-06-08 17:46:20 +0900

    feat(Tapbox): 상태관리

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:my_app/screen/home_screen.dart';
 import 'package:my_app/screen/business_card.dart';
 import 'package:my_app/screen/building_layouts.dart';
+import 'package:my_app/screen/tapboxA.dart';
+// import 'package:my_app/screen/tapboxB.dart';
+import 'package:my_app/screen/tapboxC.dart';
+
 // project_name/lib 경로를 기준으로 설정한다.
 
 void main() {
@@ -9,7 +13,7 @@ void main() {
     // runApp 다음에는 항상 MaterialApp 이 들어가야 한다.
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BuildingLayouts(),
+      home: ParentWidget(),
     ),
   );
 }
