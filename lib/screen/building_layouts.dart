68e80675d98156b66ec14c46a9fc4b94209cb0bb
jhlee-danbi <jh.lee@danbicorp.com>
2022-06-08 11:55:02 +0900

    feat(BuildingLayouts): good example

--- lib/screen/building_layouts.dart
+++ lib/screen/building_layouts.dart
@@ -1,80 +1,100 @@
 import 'package:flutter/material.dart';
 
 class BuildingLayouts extends StatelessWidget {
-  const BuildingLayouts({Key? key}) : super(key: key);
+  const BuildingLayouts({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter layout demo'),
-      ),
-      body: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(children: [
-            const TitleWidget(),
-            ButtonSection(),
-            Container(
-                child: Text(
-                    'sjkfldjsflsjdflksdjflkdsjlfgkjsdlfjsdlkjfsdklfjlskadjflskaffsjflsdahfsdhafsdafssdfsdhflsdfhlsdifhsdldhflishlfi'))
-          ])),
-    );
-  }
-
-  Container ButtonSection() {
-    return Container(
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        Column(children: [
-          Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(Icons.call, color: Colors.blue)),
-          Text('CALL', style: TextStyle(color: Colors.blue))
-        ]),
-        Column(children: [
-          Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(Icons.call, color: Colors.blue)),
-          Text('CALL', style: TextStyle(color: Colors.blue))
-        ]),
-        Column(children: [
-          Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(Icons.call, color: Colors.blue)),
-          Text('CALL', style: TextStyle(color: Colors.blue))
-        ]),
-      ]),
-    );
-  }
-}
-
-class TitleWidget extends StatelessWidget {
-  const TitleWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      // width: 280,
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
       child: Row(
         children: [
           Expanded(
+            /*1*/
             child: Column(
-              // mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Oeschinen Lake Campground'),
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
                 Text(
                   'Kandersteg, Switzerland',
-                  style: TextStyle(color: Colors.grey),
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
                 ),
               ],
             ),
           ),
-          Icon(Icons.star, color: Colors.red),
-          Text('41'),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
         ],
       ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget ButtonSection =
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      _buildButtonColumn(color, Icons.call, 'CALL'),
+      _buildButtonColumn(color, Icons.near_me, 'CALL'),
+      _buildButtonColumn(color, Icons.share, 'CALL'),
+    ]);
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Column(
+          children: [
+            Image.asset('img/lake.jpg'),
+            titleSection,
+            ButtonSection,
+            textSection
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(label,
+              style: TextStyle(
+                  fontSize: 12, fontWeight: FontWeight.w400, color: color)),
+        )
+      ],
     );
   }
 }
